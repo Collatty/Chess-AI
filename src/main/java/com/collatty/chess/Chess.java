4dab508cb3a5655fe9a523d223dee51ac041b094
Collatty <hakoncb@gmail.com>
2019-07-06 13:54:47 +0200

    Add Spring Boot dependency
    Begun implementation to allow to play my chessAI online

--- src/main/java/com/collatty/chess/Chess.java
+++ src/main/java/com/collatty/chess/Chess.java
@@ -2,13 +2,17 @@ package com.collatty.chess;
 
 import com.collatty.chess.engine.board.Board;
 import com.collatty.chess.gui.Table;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 
+@SpringBootApplication
 public class Chess {
 
     public static void main(String[] args) throws IOException {
+        SpringApplication.run(Chess.class, args);
 
-        Table.get().show();
+        //Table.get().show();
     }
 }
---
+++ src/main/java/com/collatty/chess/ChessController.java
@@ -0,0 +1,24 @@
+package com.collatty.chess;
+
+
+import com.collatty.chess.engine.board.Board;
+import com.collatty.chess.engine.player.ai.MiniMax;
+import com.collatty.chess.pgn.FenUtilities;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChessController {
+
+    @RequestMapping("/play")
+    public String playMove(@RequestParam String fen) {
+        Board board = FenUtilities.createGameFromFEN(fen);
+        MiniMax miniMax = new MiniMax(4);
+        if (board == null){
+            return "Something went wrong with the fen-string";
+        }
+        miniMax.execute(board);
+        return FenUtilities.createFENFromGame(board);
+    }
+}
